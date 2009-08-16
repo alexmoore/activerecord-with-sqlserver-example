45504c7bd1425f0572c81a3d1a153513965b3b72
Alex Moore <moore.alex@gmail.com>
2009-08-16 00:25:39 -0400

    Fixed connection issues

---
+++ Config.rb
@@ -0,0 +1,7 @@
+module Config	
+	DB_Adapter = 'sqlserver'
+	DB_Mode = 'odbc'
+	DB_DSN = 'Driver={SQL Server};Server=localhost\\SQLEXPRESS;Database=MyDatabase'
+	DB_Username = 'MyUser'
+	DB_Password = 'MyPassword'
+end
